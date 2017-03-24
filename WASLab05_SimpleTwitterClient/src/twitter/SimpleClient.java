2142cb4c44f4ebb7271652921c126e49ea88b8aa
Gerard Pradas <pradasgsj@gmail.com>
2017-03-24 18:15:07 +0100

    tasca #6 finalitzada

--- WASLab05_SimpleTwitterClient/src/twitter/SimpleClient.java
+++ WASLab05_SimpleTwitterClient/src/twitter/SimpleClient.java
@@ -1,22 +1,12 @@
 package twitter;
 
-import java.util.Date;
-
-import twitter4j.DirectMessage;
 import twitter4j.FilterQuery;
-import twitter4j.ResponseList;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
-import twitter4j.User;
-import twitter4j.UserList;
-import twitter4j.UserStreamListener;
-import twitter4j.conf.ConfigurationBuilder;
 
 public class SimpleClient {
 
@@ -24,11 +14,13 @@ public class SimpleClient {
 		
 		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
 		
-		UserStreamListener userStreamListener = new UserStreamListener() {
+		StatusListener statusListener = new StatusListener() {
 	        @Override
 	        public void onStatus(Status status) {
 	            // TODO Auto-generated method stub
-	            System.out.println(status.getText());
+	            System.out.println(status.getUser().getName() + " (@" + 
+	            status.getUser().getScreenName() + ") "  +status.getText());
+	            System.out.println();
 	        }
 
 			@Override
@@ -60,146 +52,15 @@ public class SimpleClient {
 				// TODO Auto-generated method stub
 				
 			}
-
-			@Override
-			public void onBlock(User arg0, User arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onDeletionNotice(long arg0, long arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onDirectMessage(DirectMessage arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onFavorite(User arg0, User arg1, Status arg2) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onFavoritedRetweet(User arg0, User arg1, Status arg2) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onFollow(User arg0, User arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onFriendList(long[] arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onQuotedTweet(User arg0, User arg1, Status arg2) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onRetweetedRetweet(User arg0, User arg1, Status arg2) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUnblock(User arg0, User arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUnfavorite(User arg0, User arg1, Status arg2) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUnfollow(User arg0, User arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUserDeletion(long arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUserListCreation(User arg0, UserList arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUserListDeletion(User arg0, UserList arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUserListMemberAddition(User arg0, User arg1, UserList arg2) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUserListMemberDeletion(User arg0, User arg1, UserList arg2) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUserListSubscription(User arg0, User arg1, UserList arg2) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUserListUnsubscription(User arg0, User arg1, UserList arg2) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUserListUpdate(User arg0, UserList arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUserProfileUpdate(User arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onUserSuspension(long arg0) {
-				// TODO Auto-generated method stub
-				
-			}
 		};
 				
-		twitterStream.addListener(userStreamListener);
+		twitterStream.addListener(statusListener);
 				
 		// Filter
 	    FilterQuery filtre = new FilterQuery();
-	    String[] keywordsArray = { "Barcelona"};
+	    String[] keywordsArray = {"#barcelona"};
 	    filtre.track(keywordsArray);
+	    
 	    twitterStream.filter(filtre);
 		
 	}
